8949d7b6b2cf644d3969c23efb68bb70eb8c60c0
Cricket Bird <cricketbirdy@gmail.com>
2018-11-16 05:30:51 -0500

    Done for pepin

--- 1_RunMeFirst.R
+++ 1_RunMeFirst.R
@@ -7,11 +7,10 @@ library(readr)
 
 #gender
 
-gender <- read_csv("gender.csv", col_types = cols(gender = col_factor(levels = c("female",
-                                                                                 "male"))), na = "NA")
+gender <- read_csv("data/gender.csv", col_types = cols(gender = col_factor(levels = c("female","male"))), na = "NA")
 
 #groups
-groups <- read_json("characters-groups.json") %>%
+groups <- read_json("data/characters-groups.json") %>%
       as.tibble() %>%
       unlist() %>%
       as.tibble() %>%
@@ -19,7 +18,7 @@ groups <- read_json("characters-groups.json") %>%
       rename(character = value)
 
 #scenes
-scenes <- read_csv("stranger-things.csv",
+scenes <- read_csv("data/stranger-things.csv",
                    col_types = cols(
                          sceneEnd = col_time(format = "%H:%M:%S"),
                          sceneStart = col_time(format = "%H:%M:%S")
@@ -50,3 +49,29 @@ char_scenes <- scenes  %>%
       left_join(gender) %>%
       left_join(groups)  %>%
       mutate(group = ifelse(is.na(group), "Uncredited", group))
+
+
+scene_locs <- scenes  %>%
+      select(subLocation, location, seasonNum, episode_id, scene_seq, scene_id, scene_length, scene_seq) %>%
+      distinct() %>%
+      arrange(subLocation, location, scene_id) %>%
+      group_by(subLocation, location) %>%
+      summarize(num_scenes = n_distinct(scene_seq),
+                num_episodes = n_distinct(episode_id),
+                num_seasons = n_distinct(seasonNum),
+                screen_time = as.numeric(sum(scene_length)))
+
+
+
+MainChar <- scenes %>%
+      filter(group %in% c("Main", "Party", "Upside Down") )
+
+MainCharStats <- char_scenes %>%
+      filter(group %in% c("Main", "Party", "Upside Down") )
+
+MainCharEpisodeStats <- MainChar %>%
+      group_by(character, episode_id) %>%
+      summarize(sum_screen_time = sum(scene_length)) %>%
+      mutate(episode_id = as.factor(episode_id),
+             screen_time = as.numeric(sum_screen_time))
+
